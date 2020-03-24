973833ed0c6d77cbef63e4702a2bff82b92ab19b
Filippo Falezza <filippo.falezza@outlook.it>
2020-03-24 14:51:13 +0000

    Magnetometer running on a single channel input. 10KHz sampling

---
+++ magnetometer_1channel.py
@@ -0,0 +1,22 @@
+import os,pyb,array
+adc = pyb.ADC(pyb.Pin.board.Y12)
+tim = pyb.Timer(4, freq=20000) #create 20KHz timer
+#Now it samples 4s at 10KHz
+rx0 = array.array('H', (0 for i in range(10000)))
+rx1 = array.array('H', (0 for i in range(10000)))
+rx2 = array.array('H', (0 for i in range(10000)))
+rx3 = array.array('H', (0 for i in range(10000)))
+pyb.ADC.read_timed(adc, rx0, tim)
+pyb.ADC.read_timed(adc, rx1, tim)
+pyb.ADC.read_timed(adc, rx2, tim)
+pyb.ADC.read_timed(adc, rx3, tim)
+f_ = open("/sd/magnetometer.csv", "w")
+for item in rx0:
+    f_.write('%d\n' % (item))
+for item in rx1:
+    f_.write('%d\n' % (item))
+for item in rx2:
+    f_.write('%d\n' % (item))
+for item in rx3:
+    f_.write('%d\n' % (item)))
+f_.close()
